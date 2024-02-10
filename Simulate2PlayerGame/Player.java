04961daff5950c2c8b7215a273c8276124f7e33f
Yejin Kim <124483450+yejin-km@users.noreply.github.com>
2024-02-09 23:31:26 -0500

    Update Player.java

--- Simulate2PlayerGame/Player.java
+++ Simulate2PlayerGame/Player.java
@@ -3,51 +3,51 @@ public class Player
    private String name;
    private int score;
 
- /**
-   constructor
-   @param name - the name of player
-  */
-  public Player(String name) 
-  {
-    this.name = name;
-    this.score = 0;
-  }
+/**
+constructor
+@param name - the name of player
+*/
+public Player(String name) 
+{
+this.name = name;
+this.score = 0;
+}
 
-	/**
-	 copy constructor
-	 @param player, copy of player object
-	*/
-   public Player(Player player) 
-   {
-      this.name = player.name;
-      this.score = 0;
-   }
+/**
+copy constructor
+@param player, copy of player object
+*/
+public Player(Player player) 
+{
+this.name = player.name;
+this.score = 0;
+}
 
-	/**
-  getScore method
-	@return, returns players score
-	*/
-   public int getScore() 
-   {
-      return score;
-   }
+/**
+getScore method
+@return, returns players score
+*/
+public int getScore() 
+{
+return score;
+}
 
-	/**
-   updateScore method
-	 @param score, increases score by argument value
-	*/
-   public void updateScore(int score)
-   {
-      // increase score by argument value
-      this.score += score;
-   }
+/**
+updateScore method
+@param score, increases score by argument value
+*/
+public void updateScore(int score)
+{
+// increase score by argument value
+this.score += score;
+}
 
-	/**
-   toString method
-	 @retrun name, returns players name
-	*/
-   public String toString() 
-   {
-      return name;
-   }
+/**
+toString method
+@retrun name, returns players name
+*/
+public String toString() 
+{
+return name;
+}
 }
