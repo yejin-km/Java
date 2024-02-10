36c4fe9f3eeec092b4cc0dd73b050dbb87ab2f4c
Yejin Kim <124483450+yejin-km@users.noreply.github.com>
2024-02-09 23:10:11 -0500

    Update CheckingAccount.java

--- bank savings and checking account/CheckingAccount.java
+++ bank savings and checking account/CheckingAccount.java
@@ -1,1 +1,38 @@
+//CheckingAccount inherits BaseAccount
 
+public class CheckingAccount extends BaseAccount
+{
+	//instance variable
+	private double serviceCharge;	
+	
+	//constructor
+	public CheckingAccount(double balance, int number, String owner, double serviceCharge)
+	{
+		super(balance, number, owner);
+		
+		//initialize newly added variable
+		this.serviceCharge = serviceCharge;
+	}
+
+	public void maintenance()
+	{
+		balance -= serviceCharge;
+	}
+	
+	//override withdraw()
+	public void withdraw(double amount)
+	{		
+		if(balance - amount < 0.0)
+		{
+			System.out.println("Withdrawal failed due to insufficient funds");
+		}
+		else
+			super.withdraw(amount);
+	}
+	
+	//override toString()
+	public String toString()
+	{
+		return "###CheckingsAccount summary###\nOwner: " + owner + "\nNumber: " + number + "\nBalance: $" + String.format("%.2f", balance) + "\n";
+	}
+}
